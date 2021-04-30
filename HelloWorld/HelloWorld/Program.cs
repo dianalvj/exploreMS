001cb9b969bce93d89c699af37d36809b2343b30
dianalvj <78846579+dianalvj@users.noreply.github.com>
2021-04-30 16:40:48 -0700

    Update Program.cs
    Testing commit through GH

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -7,7 +7,9 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello VS Team!");
+            // Console.WriteLine("Hello VS Team!"); 
+            // Adding comments to test commit from GH
+            
         }
     }
 }
